fd0f264d56420e9ea536fd52b857cd33b185207c
Brandon Bernard <brandon.bernard@exlrt.com>
2023-03-24 19:30:45 -0500

    Fixed Persisted Query Unit Tests now all passing (running Test application locally; separate issue in test project when deployed to Azure).

--- FlurlGraphQL.Tests/FlurlGraphQLQueryingPersistedQueryTests.cs
+++ FlurlGraphQL.Tests/FlurlGraphQLQueryingPersistedQueryTests.cs
@@ -1,8 +1,8 @@
+using System.Linq;
 using System.Threading.Tasks;
 using FlurlGraphQL.Querying.Tests.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace FlurlGraphQL.Querying.Tests
 {
@@ -10,11 +10,11 @@ namespace FlurlGraphQL.Querying.Tests
     public class FlurlGraphQLQueryingPersistedQueryTests : BaseFlurlGraphQLTest
     {
         [TestMethod]
-        public async Task TestSimpleSingleQueryDirectResultsAsync()
+        public async Task TestPersistedQuerySingleQueryDirectResultsAsync()
         {
             var results = await GraphQLApiEndpoint
-                .WithGraphQLPersistedQuery("")
-                .SetGraphQLVariables(new { ids = new[] {1000, 2001}})
+                .WithGraphQLPersistedQuery("AllCharactersWithFriendsPaginated-v1")
+                .SetGraphQLVariables(new { first = 2, friendsCount = 1})
                 .PostGraphQLQueryAsync()
                 .ReceiveGraphQLQueryResults<StarWarsCharacter>()
                 .ConfigureAwait(false);
@@ -26,13 +26,44 @@ namespace FlurlGraphQL.Querying.Tests
             Assert.IsNotNull(char1);
             Assert.AreEqual(1000, char1.PersonalIdentifier);
             Assert.AreEqual("Luke Skywalker", char1.Name);
-            Assert.IsTrue(char1.Height > (decimal)1.5);
+            var friendOfChar1 = char1.Friends.FirstOrDefault();
+            Assert.IsNotNull(friendOfChar1);
+            Assert.AreEqual("C-3PO", friendOfChar1.Name);
 
             var char2 = results[1];
             Assert.IsNotNull(char2);
-            Assert.AreEqual(2001, char2.PersonalIdentifier);
-            Assert.AreEqual("R2-D2", char2.Name);
-            Assert.IsTrue(char2.Height > (decimal)1.5);
+            Assert.AreEqual(1001, char2.PersonalIdentifier);
+            Assert.AreEqual("Darth Vader", char2.Name);
+            var friendOfChar2 = char2.Friends.FirstOrDefault();
+            Assert.IsNotNull(friendOfChar2);
+            Assert.AreEqual("Wilhuff Tarkin", friendOfChar2.Name);
+
+            var jsonText = JsonConvert.SerializeObject(results, Formatting.Indented);
+            TestContext.WriteLine(jsonText);
+        }
+
+        [TestMethod]
+        public async Task TestPersistedQueryReceiveAllPagesAsync()
+        {
+            var results = await GraphQLApiEndpoint
+                .WithGraphQLPersistedQuery("AllCharactersWithFriendsPaginated-v1")
+                .SetGraphQLVariables(new { first = 2, friendsCount = 1 })
+                .PostGraphQLQueryAsync()
+                .ReceiveAllGraphQLQueryConnectionPages<StarWarsCharacter>()
+                .ConfigureAwait(false);
+
+            Assert.IsNotNull(results);
+            Assert.IsTrue(results.Count > 1);
+            var totalCount = results.FirstOrDefault().TotalCount;
+            Assert.AreEqual(8, totalCount);
+
+            var allResults = results.SelectMany(p => p).ToList();
+            Assert.AreEqual(totalCount, allResults.Count);
+
+            foreach (var character in allResults)
+            {
+                TestContext.WriteLine($"Received Character: {character.Name}");
+            }
 
             var jsonText = JsonConvert.SerializeObject(results, Formatting.Indented);
             TestContext.WriteLine(jsonText);
